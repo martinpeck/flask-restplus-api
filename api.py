a591b3a880078025192b727907545487592a1314
Martin Peck <mpeck@hotmail.co.uk>
2018-10-18 20:06:39 +0100

    simple todo API

--- api.py
+++ api.py
@@ -1,13 +1,19 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restplus import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
-@api.route('/hello')
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
+todos = {}
+
+@api.route('/<string:todo_id>')
+class TodoSimple(Resource):
+    def get(self, todo_id):
+      return {todo_id: todos[todo_id]}
+
+    def put(self, todo_id):
+      todos[todo_id] = request.form['data']
+      return {todo_id: todos[todo_id]}
 
 if __name__ == '__main__':
     app.run(debug=True)
